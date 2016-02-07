67766f8194bf56d12aff9da70375479d9e0d4ce3
Dan Billmann <billmannd@gmail.com>
2016-02-07 17:17:49 -0500

    added website_framework directory

---
+++ website_framework/js/first_application.js
@@ -0,0 +1,21 @@
+// we will be writing a function that will be run in our HTML document.
+
+// the way to run JS in any webpage is to call on the document itself. The way
+// to do this is to call "document" into the file.
+
+document
+
+// you'll notice the color changes, indicated it already has a predetermined role
+// in JS.
+
+// To manipulate specific elements in the document, you have to grab them. There
+// are several ways to do that.
+
+//What do these mean?
+document.getElementsByClassName('className');
+document.getElementsByName('name');
+document.getElementsByTagName('tagName');
+document.getElementById('id');
+
+
+// usually we use these to get inputs from users and output the results back to them.
